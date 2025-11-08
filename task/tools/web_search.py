ecac44cded66335996fc6bef359a0766a6728a39
“Khshanovskyi” <pavlo_khshanovskyi@epam.com>
2025-11-08 17:44:40 +0200

    Initial commit

--- task/tools/web_search.py
+++ task/tools/web_search.py
@@ -13,48 +13,45 @@ class WebSearchTool(BaseTool):
 
     @property
     def name(self) -> str:
-        return 'web_search_tool'
+        return "web_search_tool"
 
     @property
     def description(self) -> str:
-        return 'Makes Search in WEB by request'
+        return "Tool for WEB searching."
 
     @property
     def input_schema(self) -> dict[str, Any]:
         return {
-            "type": "function",
-            "function": {
-                "name": "web_search_tool",
-                "description": "Tool for WEB searching.",
-                "parameters": {
-                    "type": "object",
-                    "properties": {
-                        "request": {
-                            "type": "string",
-                            "description": "The search query or question to search for on the web"
-                        }
-                    },
-                    "required": [
-                        "request"
-                    ]
+            "type": "object",
+            "properties": {
+                "request": {
+                    "type": "string",
+                    "description": "The search query or question to search for on the web"
                 }
-            }
+            },
+            "required": ["request"]
         }
 
     def execute(self, arguments: dict[str, Any]) -> str:
         headers = {
-            "Authorization": f"Bearer {self.__api_key}",
+            "Authorization": self.__api_key,
             "Content-Type": "application/json"
         }
         request_data = {
             "model": "gpt-4o-search-preview",
-            "messages": [{"role": "user", "content": str(arguments["request"])}],
-            "tools": [{"type": "static_function",
-                       "static_function": {"name": "google_search", "description": "Grounding with Google Search",
-                                           "configuration": {}}}],
-            "temperature": 0
+            "web_search_options": {},
+            "messages": [
+                {
+                    "role": "user",
+                    "content": str(arguments["request"])
+                }
+            ]
         }
-        response = requests.post(self.__endpoint, headers=headers, json=request_data)
+
+        response = requests.post(url=self.__endpoint, headers=headers, json=request_data)
+
         if response.status_code == 200:
-            return response.json()[0]["message"]["content"]
-        return f"Error: {response.json()['error']}"
+            data = response.json()
+            return data["choices"][0]["message"]["content"]
+        else:
+            return f"Error: {response.status_code} {response.text}"
